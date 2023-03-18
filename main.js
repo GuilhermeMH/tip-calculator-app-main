d02f10512b3038da77161b9e4ab6e248fd3a2172
Guilherme Moura Henrique <guilhermemoura.c710@gmail.com>
2023-03-17 22:23:13 -0300

    criação do main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const valorConta = document.querySelector('#billValue');
+const qtdPessoas = document.querySelector('#numberPeople');
+const btnGorjetas = document.querySelectorAll('#tipButtons button');
+const btnGorjetaCustom = document.querySelector('#btnTipCustom');
+const valorGorjetaPessoa = document.querySelector('#inner-tip-value');
+const valorTotalPessoa = document.querySelector('#inner-person-value');
+const alrtValorZerado = document.querySelector('#text-warning');
+const btnReset = document.querySelector('#btnReset');
+
